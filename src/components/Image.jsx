15d344642bb2eebc6b5ea5c0d1ce8cbeca8b8abc
vrajpatel <121706070+vrajpatel48@users.noreply.github.com>
2024-07-05 12:14:32 +0530

    Add files via upload

---
+++ src/components/Image.jsx
@@ -0,0 +1,44 @@
+export const skills = [
+  {
+    id: 1,
+    name: "HTML5",
+
+    image: "src/assets/html.jpeg",
+  },
+  {
+    id: 2,
+    name: "CSS3",
+
+    image: "src/assets/CSS-logo.png",
+  },
+  {
+    id: 3,
+    name: "JAVASCRIPT",
+
+    image: "src/assets/javascript.png",
+  },
+  {
+    id: 4,
+    name: "REACT JS",
+
+    image: "/src/assets/react.png",
+  },
+  {
+    id: 4,
+    name: "TAILWIND CSS",
+
+    image: "/src/assets/tailwind.jpeg",
+  },
+  {
+    id: 4,
+    name: "TYPESCRIPT",
+
+    image: "/src/assets/type.png",
+  },
+  {
+    id: 4,
+    name: "NEXT JS",
+
+    image: "/src/assets/next.jpeg",
+  },
+];
